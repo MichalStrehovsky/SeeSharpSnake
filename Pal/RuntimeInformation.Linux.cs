08cdb6479eaf1ccda151bb1a1098d9839379b3ee
Andrii Kurdiumov <kant2002@gmail.com>
2021-03-11 15:49:07 +0600

    Fix missing access modifier (#18)

--- Pal/RuntimeInformation.Linux.cs
+++ Pal/RuntimeInformation.Linux.cs
@@ -2,6 +2,6 @@ namespace System.Runtime.InteropServices
 {
     internal class RuntimeInformation
     {
-        static bool IsOSPlatform(OSPlatform platform) => platform == OSPlatform.Linux;
+        public static bool IsOSPlatform(OSPlatform platform) => platform == OSPlatform.Linux;
     }
 }
